98ead23462396a9ee56cd3f18b48b86cdf4af5d4
kevald12 <kealable@gmail.com>
2022-09-17 01:31:45 +0000

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [toDo, setToDo] = useState([]);
+  const [tarea, setTarea] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (tarea != ""){
+      setToDo((prev) => [...prev, tarea]);
+      setTarea("");
+    }
+    
+    
+  };
+  console.log(toDo);
+  const elementDellete = (dIndex) => {
+    setToDo(toDo.filter((e, i) => i != dIndex));
+  };
+
+  return (
+    <div className="container bg-dark">
+      <div>
+        <h1 className="text-center text-light">ToDo's</h1>
+        <form onSubmit={onSubmit}>
+          <div className="input-group d-flex justify-content-center">
+            <input
+              type="text"
+              placeholder="Tareas a realizar"
+              value={tarea}
+              onChange={(e) => setTarea(e.target.value)}
+            />
+          </div>
+          <div>
+            {toDo.map((element, dIndex) => {
+              return (
+                <div className="d-flex justify-content-between my-3 col-4 mx-auto shadow p-3 mb-5 bg-body rounded">
+                  <p>{element}</p>
+                  <button
+                    type="button"
+                    className="btn btn-danger opacity-50"
+                    onClick={() => elementDellete(dIndex)}
+                  >
+                    Borrar
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
